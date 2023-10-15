7760a34e6d60b683615c793fcedca49d9f2913d2
kunal singh <75198769+kunalsingh04@users.noreply.github.com>
2023-10-15 16:22:57 +0530

    resume

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -4,7 +4,7 @@ import myImg from "../../Assets/123.jpg";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
-  AiOutlineTwitter,
+  //AiOutlineTwitter,
   AiFillInstagram,
 } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
